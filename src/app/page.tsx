23d74548aabad5feb8450d26a09004327fea9898
Gaurav Gupta <113352611+Gauravgupta2026@users.noreply.github.com>
2025-01-30 16:21:27 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { FeatureSection } from "@/components/features-section";
 import Footer from "@/components/footer-section";
 import Hero from "@/components/hero-section";
-import Logo, { LogoSection } from "@/components/logo-section";
+import Logo from "@/components/logo-section";
 import Navigation from "@/components/navbar-section";
 
 
